b6d10a8ef9f2227b9230e6cab48aefee71cc16b6
Antonio Caro <antoniojosecaropozo@gmail.com>
2024-09-23 14:13:15 +0200

    feat: main class.

---
+++ src/main/java/es/ieslosmontecillos/helloworldjavafxstyle/HelloWorld.java
@@ -0,0 +1,40 @@
+//This code belongs to a tutorial from https://docs.oracle.com/javase/8/javafx/get-started-tutorial/get_start_apps.htm
+//The whole point of this code is showing how to create a window with a button and using that button in order to console log "Hello world"
+package es.ieslosmontecillos.helloworldjavafxstyle;
+
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloWorld extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
